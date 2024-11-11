74f61f22eb623b369a5c261f851f14b4b752ac0f
wisdom Agha <wisdomcool11@gmail.com>
2024-11-11 01:11:43 +0100

    first commit: committing all files to repo

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+console.log('hello')
+
+const header = document.querySelector('header');
+const toggle_bar = document.querySelector('.toggle_bar');
+
+
+toggle_bar.addEventListener('click' , function(){
+    header.classList.toggle("active");
+})
+
+
+
+// Scroll Reveal
+ScrollReveal({ 
+    reset: true, 
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('header', {delay: 200, origin: 'top'});
+ScrollReveal().reveal('#hero__text, #hero__content,#CTA__btn',{delay: 700, origin: 'left', interval: 400 })
+ScrollReveal().reveal('#card__one, #card__two',{delay: 500, origin: 'bottom' , interval: 200 })
+
+
+
